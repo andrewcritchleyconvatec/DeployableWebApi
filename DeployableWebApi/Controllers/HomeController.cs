5587d4e0c32817fbf7ad4dca093042bdc4b66242
Andrew Critchley <andrew.critchley@convatec.com>
2019-04-08 15:44:57 +0100

    Added a custom label (overridable in the web config)

--- DeployableWebApi/Controllers/HomeController.cs
+++ DeployableWebApi/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace DeployableWebApi.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            ViewBag.CustomLabel = ConfigurationManager.AppSettings["CustomLabel"];
 
             return View();
         }
