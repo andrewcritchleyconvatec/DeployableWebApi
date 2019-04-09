e043927ff82eba0b67c50b6e8eb623de8e42c7fe
Andrew Critchley <andrew.critchley@convatec.com>
2019-04-09 12:31:15 +0100

    Added basic serilog logs.

--- DeployableWebApi/Controllers/HomeController.cs
+++ DeployableWebApi/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace DeployableWebApi.Controllers
 {
@@ -15,6 +14,44 @@ namespace DeployableWebApi.Controllers
             ViewBag.CustomLabel = ConfigurationManager.AppSettings["CustomLabel"];
 
             return View();
+        }
+
+
+        public ActionResult CustomSuperAction(string query)
+        {
+            var logger = CreateLogger();
+
+            logger.Information("The user asked for {query}", query);
+
+            try
+            {
+                if (query == "break")
+                    throw new ArgumentException($"I don't like the query {query}");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Query {query} is not supported.");
+            }
+            return Content("This is some content");
+        }
+
+        private ILogger CreateLogger()
+        {
+            var environment = ConfigurationManager.AppSettings["Environment"];
+            var systemName = "AndrewsSystem";
+            var instanceName = "AndrewsSystem.Api";
+            var logFilePath = $@"C:\Logs\splunk\{environment}\{systemName}\{instanceName}\log.txt";
+
+            var log = new LoggerConfiguration()
+                .Enrich.WithProperty("Environment", environment)
+                .Enrich.WithProperty("SystemName", systemName)
+                .Enrich.WithProperty("InstanceName", instanceName)
+                .Enrich.FromLogContext()
+                .WriteTo.File(new JsonFormatter(renderMessage: true), logFilePath)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            return log;
         }
     }
 }
